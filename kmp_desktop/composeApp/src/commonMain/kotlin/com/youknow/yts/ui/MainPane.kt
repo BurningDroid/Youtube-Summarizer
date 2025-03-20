2401da6533ff52a32142424cb090fd7d43f93c3b
Dev.Aaron <youknow.yoonho@gmail.com>
2025-03-20 21:59:11 +0900

    youtube 영상 다운로드 로직 구현

--- kmp_desktop/composeApp/src/commonMain/kotlin/com/youknow/yts/ui/MainPane.kt
+++ kmp_desktop/composeApp/src/commonMain/kotlin/com/youknow/yts/ui/MainPane.kt
@@ -8,6 +8,10 @@ import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -36,9 +40,8 @@ fun MainPane(
         )
 
         Button(
-            onClick = {
-
-            }
+            onClick = vm::onClickRun,
+            enabled = vm.enableRunButton
         ) {
             Text("Run")
         }
--- kmp_desktop/composeApp/src/commonMain/kotlin/com/youknow/yts/ui/MainViewModel.kt
+++ kmp_desktop/composeApp/src/commonMain/kotlin/com/youknow/yts/ui/MainViewModel.kt
@@ -4,13 +4,37 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.youknow.yts.ytdlp.YtDlp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val ytDlp: YtDlp = YtDlp()
+) : ViewModel() {
 
     var youtubeUrl: String by mutableStateOf("")
+        private set
+
+    var enableRunButton: Boolean by mutableStateOf(true)
         private set
 
     fun onInputUrl(url: String) {
         youtubeUrl = url
     }
+
+    fun onClickRun() {
+        if (youtubeUrl.isEmpty()) {
+            return
+        }
+
+        enableRunButton = false
+
+        viewModelScope.launch {
+            ytDlp.download(youtubeUrl)
+
+            enableRunButton = true
+        }
+    }
+
 }
