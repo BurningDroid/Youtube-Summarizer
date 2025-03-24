03a900cfcc47ba9edcbd0126c6413dc80e05659e
Dev.Aaron <youknow.yoonho@gmail.com>
2025-03-24 09:11:49 +0900

    navigation 의존성 추가

--- kmp_desktop/composeApp/build.gradle.kts
+++ kmp_desktop/composeApp/build.gradle.kts
@@ -37,6 +37,8 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.viewmodel.compose)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.androidx.navigation.compose)
+
 
             // Ktor
             implementation(libs.ktor.client.cio)
