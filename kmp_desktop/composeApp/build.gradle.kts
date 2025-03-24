d04c3ea3e7cedbd703784c944b4c26f774251bd2
Dev.Aaron <youknow.yoonho@gmail.com>
2025-03-25 00:07:41 +0900

    Update build.gradle.kts

--- kmp_desktop/composeApp/build.gradle.kts
+++ kmp_desktop/composeApp/build.gradle.kts
@@ -39,8 +39,9 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.viewmodel.compose)
             implementation(libs.androidx.lifecycle.runtime.compose)
-            implementation(libs.androidx.navigation.compose)
 
+            // Navigation
+            implementation(libs.androidx.navigation.compose)
 
             // Ktor
             implementation(libs.ktor.client.cio)
